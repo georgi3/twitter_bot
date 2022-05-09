99d4ba53887c6d23ead7a896abca2eb217259b75
George Marchenko <george.marchenko44@gmail.com>
2022-05-09 09:31:39 -0400

    wrote the code that unfollows users who are not friends

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+API_KEY = os.environ['GAMECAP_API_KEY']
+API_SECRET_KEY = os.environ['GAMECAP_SECRET_API']
+BEARER_TOKEN = os.environ['GAMECAP_BEARER_TOKEN']
+ACCESS_TOKEN = os.environ['GAMECAP_ACCESS_TOKEN']
+ACCESS_TOKEN_SECRET = os.environ['GAMECAP_ACCESS_TOKEN_SECRET']
+EXCEPTIONS = ['worldofwomennft', 'NFTCompanyy', 'cyphernormie', 'XtrangeArtist', 'EmotionalLadies',
+             'QueenRusskiy', 'CleverGlitch', 'Elonmusk', 'cryptodaily', 'opensea', 'etheretro', 'Zeneca_33']
+SCREEN_NAME = 'gamecapNFT'
