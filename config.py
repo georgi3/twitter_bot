a14fa464d1521dfa710ec4ca69f6b162259e6972
George Marchenko <george.marchenko44@gmail.com>
2022-05-09 11:42:30 -0400

    added stopper not to get suspended

--- config.py
+++ config.py
@@ -9,3 +9,13 @@ EXCEPTIONS = ['worldofwomennft', 'NFTCompanyy', 'cyphernormie', 'XtrangeArtist',
              'QueenRusskiy', 'CleverGlitch', 'Elonmusk', 'cryptodaily', 'opensea', 'etheretro', 'Zeneca_33']
 SCREEN_NAME = 'gamecapNFT'
 
+## DOUBLE CHECK THEM
+COMMENT_POOL = ["Please follow our NFT account, we are donating all proceeds to Ukrainian’s in need!",
+                "Follow our NFT account created by Ukrainians, to help Ukrainian’s.",
+                "Follow our NFT account to help Ukrainians, our collection is dropping soon!",
+                "Hello my name is Gosha Marchenko and I made an NFT account to help my country, Ukraine. Please follow"
+                "Follow for follow!",
+                "As the war on Ukraine continues, we are helping those suffering! Follow us for more.",
+                "Check out my little sister's NFT collection about Ukraine!",
+                "My family was forced to flee Ukraine, please check out my NFT page to see how we are doing our part,"
+                " and how you can help!"]
