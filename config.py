a90e7e34269f5c26c34f1249b7707c16dc0eb002
George Marchenko <george.marchenko44@gmail.com>
2022-05-09 23:03:02 -0400

    updated the settings

--- config.py
+++ config.py
@@ -5,17 +5,11 @@ API_SECRET_KEY = os.environ['GAMECAP_SECRET_API']
 BEARER_TOKEN = os.environ['GAMECAP_BEARER_TOKEN']
 ACCESS_TOKEN = os.environ['GAMECAP_ACCESS_TOKEN']
 ACCESS_TOKEN_SECRET = os.environ['GAMECAP_ACCESS_TOKEN_SECRET']
-EXCEPTIONS = ['worldofwomennft', 'NFTCompanyy', 'cyphernormie', 'XtrangeArtist', 'EmotionalLadies',
-             'QueenRusskiy', 'CleverGlitch', 'Elonmusk', 'cryptodaily', 'opensea', 'etheretro', 'Zeneca_33']
-SCREEN_NAME = 'gamecapNFT'
 
-## DOUBLE CHECK THEM
-COMMENT_POOL = ["Please follow our NFT account, we are donating all proceeds to Ukrainian’s in need!",
-                "Follow our NFT account created by Ukrainians, to help Ukrainian’s.",
-                "Follow our NFT account to help Ukrainians, our collection is dropping soon!",
-                "Hello my name is Gosha Marchenko and I made an NFT account to help my country, Ukraine. Please follow"
-                "Follow for follow!",
-                "As the war on Ukraine continues, we are helping those suffering! Follow us for more.",
-                "Check out my little sister's NFT collection about Ukraine!",
-                "My family was forced to flee Ukraine, please check out my NFT page to see how we are doing our part,"
-                " and how you can help!"]
+TARGET_AUDIENCE = []
+TARGET_WORDS = []
+# users to keep following even if they do not follow you
+EXCEPTIONS = []
+# your twitter handle
+SCREEN_NAME = ''
+
